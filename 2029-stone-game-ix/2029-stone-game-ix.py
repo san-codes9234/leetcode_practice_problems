43b970c6178c241193dc9db3d4a70f157e3d26d5
SANKALP_KUMAR <choudharysankalp530@gmail.com>
2026-08-16 20:14:02 +0530

    Time: 43 ms (88.55%), Space: 30.4 MB (88.55%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        c = [0, 0, 0]
+        for s in stones:
+            c[s % 3] += 1
+        
+        if c[0] % 2 == 0:
+            return c[1] > 0 and c[2] > 0
+        else:
+            return abs(c[1] - c[2]) > 2
